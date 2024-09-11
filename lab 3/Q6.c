38dc3031f733988643e993e926e958a087981e34
Mr.Adnan <126695322+adnanTheSmasher@users.noreply.github.com>
2024-09-11 10:22:38 +0500

    Update Q6.c

--- lab 3/Q6.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
- float x1 = 5, y1 = 4, x2 = 3, y2 = 2;
-  float slope;
-  slope = (y2-y1)/(x2-x1);
-  printf("the slope of point (%.3f, %.3f) and (%.3f, %.3f is: %.3f", x1,y1,x2,y2,slope);
-}
